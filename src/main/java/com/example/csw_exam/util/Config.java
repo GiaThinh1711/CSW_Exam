abe6a89ac2b563e4f882f097bf3cb45b190feffb
Admin <thinhdau1711@gmail.com>
2022-06-10 09:00:00 +0700

    gg

---
+++ src/main/java/com/example/csw_exam/util/Config.java
@@ -0,0 +1,8 @@
+package com.example.csw_exam.util;
+
+public class Config {
+    public static final String DATABASE_DRIVE_CLASS = "com.mysql.jdbc.Driver";
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/cws_exam";
+    public static final String DATABASE_USERNAME = "root";
+    public static final String DATABASE_PASSWORD = "";
+}
