d678e42df382bd698a0a40cca7246c26542dce83
Admin <thinhdau1711@gmail.com>
2022-06-10 09:19:39 +0700

    gg

--- src/main/java/com/example/csw_exam/util/SqlConfig.java
+++ src/main/java/com/example/csw_exam/util/SqlConfig.java
@@ -4,6 +4,6 @@ public class SqlConfig {
     public static final String INSERT_EMPLOYEE = "INSERT INTO employee (name,salary) VALUES (?,?)";
     public static final String LIST_EMPLOYEE = "SELECT * FROM employee";
     public static final String UPDATE_EMPLOYEE =
-            "UPDATE employee SET Id=?,name=?,salary=? WHERE id = ?";
+            "UPDATE employee SET id=?,name=?,salary=? WHERE id = ?";
     public static final String DETAIL_EMPLOYEE = "select * from employee where id = ?";
 }
